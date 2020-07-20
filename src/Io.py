dde7cb24c2f819b1972ddd568729b98aa236056b
hamidoullah <hamidoullah.nour-omar@epitech.eu>
2020-07-20 05:04:43 +0200

    io modified

--- src/Io.py
+++ src/Io.py
@@ -2,42 +2,40 @@ import glob
 import pathlib
 import json
 import numpy as np
+import pickle
 
 class Io:
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	@staticmethod
-	def get_images(path : str) -> list:
-		d = glob.glob(path)
-		if len(d) == 0:
-			raise Exception("Path does not exist.")
-		return d
+    @staticmethod
+    def get_images(path : str) -> list:
+        d = glob.glob(path)
+        if len(d) == 0:
+            raise Exception("Path does not exist.")
+        return d
 
-	@staticmethod
-	def write_data_to_file(data : tuple, path : str ="results.txt"):
-		result_file = open(path, 'w')
-		for var in data:
-			if type(var) is np.ndarray:
-				var = var.tolist()
-			result_file.write(json.dumps(var) + '\n')
-		result_file.close()
+    @staticmethod
+    def write_data_to_file(data : tuple, path : str ="results.npy"):
+        np.save(path, data)
 
-	@staticmethod
-	def get_data_from_file(path : str = "results.txt") -> list:
-		a = []
-		result_file = open(path, 'r')
-		for line in result_file:
-			a.append(json.loads(line.strip('\n')))
-		result_file.close()
-		return a
+    @staticmethod
+    def get_data_from_file(path : str = "results.npy") -> list:
+        a = np.load(path)
+        return a
 
 
 if __name__ == "__main__":
-	ar = np.array([1, 2, 3])
-	sec_arr = np.array([[1, 2, 3], [0, 5, 8]])
-	b = Io()
-	i = b.get_images("./*.py")
-	b.write_data_to_file((ar, sec_arr, [1, 2, 3], {'Ok': 1, 'oook': 9}))
-	a = b.get_data_from_file()
-	print(a)
+    ar = np.array([1, 2, 3])
+    sec_arr = np.array([[1, 2, 3], [0, 5, 8]])
+    b = Io()
+    i = b.get_images("./*.py")
+    b.write_data_to_file((ar, sec_arr, [np.array([[ 5.7347458 ],
+       [-2.70714913],
+       [14.02886978]]), np.array([[ 5.09454526],
+       [-2.62515634],
+       [14.86642347]]), np.array([[ 3.05927455],
+       [-2.48136364],
+       [ 9.46724591]])]))
+    a = b.get_data_from_file()
+    print(a)
